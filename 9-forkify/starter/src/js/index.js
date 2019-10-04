fc56c75103d61bb8edaae4d33581b157ee5d402f
Vitaly Cherkasov <ftt.vitaly.cherkasov@grabtaxi.com>
2019-10-03 23:14:51 -0700

    1. Added title length limitation in results 2. Added loading spinner 3. Added pagination and page buttons

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import * as searchView from './views/searchView'
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 /* Global state of the app
 - Search Object
@@ -23,11 +23,13 @@ const controlSearch = async () => {
     //3. Prepare UI for Results
     searchView.clearInput();//clear input field
     searchView.clearResults();//clear results
+    renderLoader(elements.searchRes);//loading spinner
 
     //4. Search for recipes
     await state.search.getResults();// wait for result from getResults()
 
     //5. Render results on UI
+    clearLoader();//remove spinner
     searchView.renderResults(state.search.result);//render all recepies and show them on the webpage
   }
 }
@@ -39,3 +41,13 @@ elements.searchForm.addEventListener('submit', e => {
   //Clicking on a link, prevent the link from following the URL.
   controlSearch();
 });
+
+elements.searchResPages.addEventListener('click', e => {
+  const btn = e.target.closest('.btn-inline')
+  if (btn){
+    const goToPage = parseInt(btn.dataset.goto, 10) //dataset reads data-goto=${type === 'prev' ? page-1 : page+1}
+    //10 means from 0 to 9
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage); //load new page with the rest of results
+  }
+})
--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -3,9 +3,27 @@ export const getInput = () => elements.searchInput.value;//get value from the se
 export const clearInput = () => {
   elements.searchInput.value=''
 };
+
 export const clearResults = () => {
-  elements.searchResList.innerHTML = '';//clear HTML
-}
+  elements.searchResList.innerHTML = ' ';//clear results HTML
+  elements.searchResPages.innerHTML = ' ';//clear buttons HTML
+};
+
+const limitRecipeTitle = (title, limit=17) => { //limit results to one line in the results column
+  const newTitle = [];
+  if (title.length > limit){
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit){
+        newTitle.push(cur);
+      }
+      return acc + cur.length; //to update accumulator
+    }, 0);
+    //return the result
+    return `${newTitle.join(' ')}...`; //concure array in string by space and add ... to the end
+  }
+    return title; //just return title if it's < limit
+};
+
 const renderRecipe = recipe => {
   const markup = `
   <li>
@@ -14,7 +32,7 @@ const renderRecipe = recipe => {
               <img src="${recipe.image_url}" alt="${recipe.title}">
           </figure>
           <div class="results__data">
-              <h4 class="results__name">${recipe.title}</h4>
+              <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
               <p class="results__author">${recipe.publisher}</p>
           </div>
       </a>
@@ -22,6 +40,45 @@ const renderRecipe = recipe => {
   `;//adding HTML with images and names into the page
   elements.searchResList.insertAdjacentHTML('beforeend', markup)
 };
-export const renderResults = recipes => {
-  recipes.forEach(renderRecipe);//loop thru all recipes
+
+//Adding HTML Prev and Next buttons
+//type: 'prev' or 'next'
+const createButton = (page, type) => `
+<button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page-1 : page+1}>
+<span>Page ${type === 'prev' ? page-1 : page+1}</span>
+    <svg class="search__icon">
+        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+    </svg>
+</button>
+`; //if button is for previous page, we show cur page-1 num if for next cur page+1
+
+const renderButtons = (page, numResults, resPerPage) => { //choose what buttons to show and call button HTML
+  const pages = Math.ceil(numResults/resPerPage); //Math.ceil to round 4,1 or 4,5 to 5
+
+  let button;
+  if(page === 1 && pages > 1){
+    //Only button to go to the next page
+    button = createButton(page, 'next');
+  }else if(page < pages){
+    //Both buttons
+    button = `
+    ${createButton(page, 'next')}
+    ${createButton(page, 'prev')}
+    `;
+  }else if (page === pages && pages > 1){
+    //Only button to go to prev page
+    button = createButton(page, 'prev');
+  }
+  elements.searchResPages.insertAdjacentHTML('afterbegin', button)
+};
+
+export const renderResults = (recipes, page=1, resPerPage=10) => {
+  //render results of current page
+  const start = (page-1) * resPerPage;
+  const end = page * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);//loop thru all recipes
+
+  //render pagination buttons
+  renderButtons(page, recipes.length, resPerPage);
 };
