206c06cb61f7ea3c013ce1b5d5221811ed934dc4
Vitaly Cherkasov <ftt.vitaly.cherkasov@grabtaxi.com>
2019-11-21 00:01:44 -0800

    Added Shopping list Moodel and Controller

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView'
 import {elements, renderLoader, clearLoader} from './views/base';
@@ -123,3 +124,5 @@ elements.recipe.addEventListener('click', e => {
   }
 
 });
+
+window.l = new List();
---
+++ 9-forkify/starter/src/js/models/List.js
@@ -0,0 +1,65 @@
+import uniqid from 'uniqid';
+
+export default class List {
+  constructor(){
+    this.items = [];
+  }
+
+  addItem(count, unit, ingredient){
+    const item = {
+      id: uniqid(), //using uniqid from uniqid package to have qnique id for each element
+      count, //or: count: count;
+      unit,
+      ingredient
+    }
+    this.items.push(item);
+    return item
+  }
+    deleteItem(id){
+      const index = this.items.findIndex(el => el.id === id);
+
+      // [2,4,8] splice(1,1) start at position 1 and take 1 element -> returns 4, original array is [2,8] - mutates arruy
+      // [2,4,8] slice(1,1) returns 4, original array is [2,4,8] - doesn't mutate array
+      this.items.splice(index, 1);
+    }
+
+    updateCount(id, newCount){
+      this.items.find(el => el.id === id).count = newCount //will return element itself, not index and then change the count property
+    }
+  }
+
+/* Map will be better soluton
+import uniqid from 'uniqid';
+
+export default class ShoppingList {
+    constructor(){
+        this.list = new Map();
+    };
+
+    add_item(count,unit,ingredient){
+        this.list.set(uniqid(),{
+            count : count,
+            unit : unit,
+            ingredient : ingredient
+        });
+    };
+
+    delete_item(id){
+        if (this.list.has(id)){
+            this.list.delete(id)
+        };
+    };
+
+    update_count(id,count){
+        let value ;
+        if (this.list.has(id)){
+            value = this.list.get(id);
+            value.count = count;
+            this.list.set(id,value);
+        };
+    };
+};
+
+
+a typical list deletion or update operation takes O(N) times however a Map takes O(1)
+*/
